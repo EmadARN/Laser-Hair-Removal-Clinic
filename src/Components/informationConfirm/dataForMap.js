ebdef10259e81c3ede21d87b73f711ad946bb486
RezaFarzipour <rezafafa78@gmail.com>
2024-06-09 17:11:02 +0330

    data for user confirm

---
+++ src/Components/informationConfirm/dataForMap.js
@@ -0,0 +1,15 @@
+export const dataForMap = [
+  { id: 1, title: "نام", value: "نام مرجع" },
+  { id: 2, title: "نام خانوادگی", value: "نام خانوادگی" },
+  { id: 3, title: "شماره موبایل", value: "09190978042" },
+  { id: 4, title: "تلفن ثابت", value: "024123455" },
+  { id: 5, title: "کدملی", value: "0312028156" },
+  { id: 6, title: "دارو مصرفی", value: "" },
+  { id: 7, title: "سابقه بیماری", value: "" },
+];
+
+export const TurnData = [
+  { id: 1, title: "روز و تاریخ", value: "سه شنبه ۲۶/۱۲/۱۴۰۲" },
+  { id: 2, title: "بازه زمانی", value: "9-11.30" },
+  { id: 3, title: "اپراتور", value: "نام اپراتور" },
+];
