37cd8472d8733109685efe22a6f6a639eff23952
emad <arabanemad@gmail.com>
2024-07-06 15:59:16 +0330

    Create AcceptBtn & Stepper & globalsCss

---
+++ src/Common/stepper/Stepper.jsx
@@ -0,0 +1,58 @@
+import {
+  Box,
+  Step,
+  StepDescription,
+  StepIndicator,
+  StepStatus,
+  Stepper,
+  useSteps,
+} from "@chakra-ui/react";
+
+const steps = [
+  { description: "انتخاب نواحی" },
+  { description: "تاریخ و ساعت " },
+  { description: "اطلاعات مراجع" },
+  { description: "تایید و پرداخت " },
+];
+
+const StepperPrototype = () => {
+  const { activeStep } = useSteps({
+    index: 1,
+    count: steps.length,
+  });
+
+  return (
+    <Stepper
+      sx={{
+        w: "50%",
+        m: "auto",
+      }}
+      size="lg"
+      colorScheme="yellow"
+      index={activeStep}
+    >
+      {steps.map((step, index) => (
+        <Step key={index}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+            }}
+          >
+            <StepIndicator>
+              <StepStatus complete={`✅`} incomplete={`😅`} active={`📍`} />
+            </StepIndicator>
+
+            <Box>
+              <StepDescription>{step.description}</StepDescription>
+            </Box>
+          </Box>
+        </Step>
+      ))}
+    </Stepper>
+  );
+};
+
+export default StepperPrototype;
