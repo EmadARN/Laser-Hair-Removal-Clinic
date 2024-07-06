d3d43980088c8fadd2cc4d1f2251387c71869858
emad <arabanemad@gmail.com>
2024-07-06 16:05:43 +0330

    accordionMenu & acceptBtn & stepper & TitleUser

---
+++ src/Common/acceptBtn/AcceptBtn.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@chakra-ui/react";
+import React from "react";
+
+export const AcceptBtn = () => {
+  return (
+    <Button w={"full"} mt={4}>
+      ادامه
+    </Button>
+  );
+};
---
+++ src/Common/titleUserDashboard/TitleUserDashboard.jsx
@@ -0,0 +1,26 @@
+import {  Flex, Text } from "@chakra-ui/react";
+import React from "react";
+import { RiHome5Line } from "react-icons/ri";
+import { IoIosArrowRoundBack } from "react-icons/io";
+const TitleUserDashboard = () => {
+  return (
+    <Flex justifyContent="space-between" bgColor="#1111">
+      <Text sx={{ display: "flex", alignItems: "center", gap: "5px" }}>
+        <span>
+          <RiHome5Line size="24px" />
+        </span>
+        بازگشت به خانه
+      </Text>
+      <Text
+        sx={{ display: "flex", alignItems: "center", position: "relative" }}
+      >
+        مرحله قبل
+        <span style={{ paddingTop: "5px" }}>
+          <IoIosArrowRoundBack size="24px" />
+        </span>
+      </Text>
+    </Flex>
+  );
+};
+
+export default TitleUserDashboard;
