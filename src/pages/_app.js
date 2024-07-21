5f3244bac0c6d79caf8d319a548ada2c22bfa6db
emad <arabanemad@gmail.com>
2024-07-21 12:11:56 +0330

    Update userDashboard

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
+import { DashboardContextProvider } from "@/context/DashboardContext";
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <DashboardContextProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </DashboardContextProvider>
   );
 }
