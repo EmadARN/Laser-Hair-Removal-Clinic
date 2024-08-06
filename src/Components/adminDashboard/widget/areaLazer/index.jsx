a22c0020567a92a8238e378a45aeb6e6a90d9ff5
emad <arabanemad@gmail.com>
2024-08-06 23:37:20 +0330

    Create areaLazer & clients

---
+++ src/Components/adminDashboard/widget/areaLazer/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Lists from "../../common/Lists";
+import ModalAttention from "@/Common/moadals/ModalAttention";
+import ModalDefine from "@/Common/moadals/ModalDefine";
+import ModalBodyContent from "./widget/modalDefineDetails/ModalBodyContent";
+import ModalFooterContent from "./widget/modalDefineDetails/ModalFooterContent";
+import HeaderContent from "./widget/modalAttentionDetails/HeaderContent";
+import BodyContent from "./widget/modalAttentionDetails/BodyContent";
+import FooterContent from "./widget/modalAttentionDetails/FooterContent";
+
+const AreaLazer = () => {
+  return (
+    <>
+      <ModalDefine
+        headerContent="افزودن ناحیه جدید"
+        renderContent={() => ({
+          body: <ModalBodyContent />,
+          footer: <ModalFooterContent />,
+        })}
+      />
+      <ModalAttention
+        renderContent={() => ({
+          header: <HeaderContent />,
+          body: <BodyContent />,
+          footer: <FooterContent />,
+        })}
+      />
+      <Lists
+        firstArea="فول بادی"
+        secondArea="450,000 تومان"
+        thirdArea="35 دقیقه"
+        imgDisplay="none"
+      />
+    </>
+  );
+};
+
+export default AreaLazer;
--- src/Components/adminDashboard/widget/employees/index.jsx
+++ src/Components/adminDashboard/widget/employees/index.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import ModalDefine from "@/Common/moadals/ModalDefine";
 import ModalAttention from "@/Common/moadals/ModalAttention";
-import ListEmployee from "./widget/ListEmployee";
 import ModalBodyContent from "./widget/modalDefineDetails/ModalBodyContent";
 import ModalFooterContent from "./widget/modalDefineDetails/ModalFooterContent";
 import BodyContent from "./widget/modalAttentionDetails/BodyContent";
 import FooterContent from "./widget/modalAttentionDetails/FooterContent";
 import HeaderContent from "./widget/modalAttentionDetails/HeaderContent";
+import Lists from "../../common/Lists";
 
 const Empolyees = () => {
   return (
     <>
       <ModalDefine
+        headerContent="افزودن کارمند جدید"
         renderContent={() => ({
           body: <ModalBodyContent />,
           footer: <ModalFooterContent />,
@@ -24,7 +25,7 @@ const Empolyees = () => {
           footer: <FooterContent />,
         })}
       />
-      <ListEmployee />
+      <Lists  firstArea="علی مظفری" secondArea="منشی" displayThirdArea="none"  />
     </>
   );
 };
