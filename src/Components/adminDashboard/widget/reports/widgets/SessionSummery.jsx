922d914aa8e9bbcd6eec9dc3f1f85f1b80c063de
RezaFarzipour <rezafafa78@gmail.com>
2024-08-10 12:29:18 +0330

    Session record has been maded in admin adminDashb

---
+++ src/Components/adminDashboard/widget/reports/widgets/SessionSummery.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import SessionSummeryBox from "./SessionSummeryBox";
+import { HiMiniUserGroup } from "react-icons/hi2";
+import { FaWallet } from "react-icons/fa";
+import { Box } from "@chakra-ui/react";
+const SessionSummery = () => {
+  return (
+    <Box
+      width={"100%"}
+      display={"flex"}
+      justifyContent={"space-between"}
+      flexDirection={{base:"column",sm:"row"}}
+      alignItems={"center"}
+      gap={{base:4,sm:"0"}}
+    >
+      <Box width={{base:"100%",sm:"40%",md:"35%"}}>
+        <SessionSummeryBox
+        number={0}
+          icon={<HiMiniUserGroup size={"20px"} color="blue" />}
+          title="تعداد جلسات لیزر"
+        />
+      </Box>
+
+      <Box width={{base:"100%",sm:"40%",md:"35%"}}>
+        <SessionSummeryBox
+        number={0}
+          icon={<FaWallet size={"20px"} color="blue" />}
+          title=" درامد"
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default SessionSummery;
