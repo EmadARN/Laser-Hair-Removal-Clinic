11200d51a5cd6d8f449f269a6385cdc8b7c8a146
emad <arabanemad@gmail.com>
2024-10-07 13:41:20 +0330

    Add header Home

---
+++ src/Components/home/header/widget/HeaderBtn.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Box, keyframes } from "@chakra-ui/react";
+
+const shimmer = keyframes`
+  0% {
+    background-position: -200% 0;
+  }
+  100% {
+    background-position: 200% 0;
+  }
+`;
+
+const HeaderBtn = () => {
+  return (
+    <Box
+      as="button"
+      h="50px"
+      px="24px"
+      borderRadius="md"
+      border="1px solid"
+      borderColor="brand.400"
+      backgroundImage="linear-gradient(110deg, brand.400 45%, #b57edc 55%, brand.400 )"
+      backgroundSize="200% 100%"
+      animation={`${shimmer} 5s linear infinite`}
+      color="white"
+      fontWeight="medium"
+      transition="background-color 0.2s"
+      _hover={{ backgroundColor: "rgba(181, 126, 220, 0.7)" }}
+      position={"absolute"}
+      top={{ base: "115%", md: "80%" }}
+      left="50%"
+      transform="translate(-50%, -50%)"
+      whiteSpace={"nowrap"}
+    >
+      همین حالا نوبت رزرو کن
+    </Box>
+  );
+};
+
+export default HeaderBtn;
---
+++ src/Components/home/header/widget/TextEffect.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { Box, Text } from "@chakra-ui/react";
+
+export const TextGenerateEffect = ({
+  words,
+  filter = true,
+  duration = 0.1,
+}) => {
+  const [scope, animate] = useAnimate();
+  const [isMounted, setIsMounted] = useState(false); // برای کنترل رندر اولیه
+  let wordsArray = words.split(" ");
+
+  useEffect(() => {
+    setIsMounted(true); // کامپوننت در سمت کلاینت بارگذاری شد
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      animate(
+        "span",
+        {
+          opacity: 1,
+          filter: filter ? "blur(0px)" : "none",
+        },
+        {
+          duration: duration,
+          delay: stagger(0.1),
+        }
+      );
+    }
+  }, [scope.current, isMounted]);
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope}>
+        {wordsArray.map((word, idx) => {
+          return (
+            <motion.span
+              key={word + idx}
+              style={{
+                filter: filter ? "blur(10px)" : "none",
+                opacity: 0, // رندر اولیه با opacity صفر
+              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+            >
+              {word}{" "}
+            </motion.span>
+          );
+        })}
+      </motion.div>
+    );
+  };
+
+  return (
+    <Box fontWeight="bold">
+      <Box mt={4}>
+        <Text
+          color="gray.600"
+          fontSize="15px"
+          lineHeight="snug"
+          letterSpacing="wide"
+        >
+          {isMounted && renderWords()}{" "}
+          {/* فقط در صورت بارگذاری در کلاینت رندر شود */}
+        </Text>
+      </Box>
+    </Box>
+  );
+};
