8eeaa2c489bb842cb31e67d6141346e9f16f48e7
emad <arabanemad@gmail.com>
2024-10-26 17:37:40 +0330

    Update PatientWithoutTime

--- src/Components/reseptionDashboard/widget/AddPatientWithoutTime/PatientWithoutTime.jsx
+++ src/Components/reseptionDashboard/widget/AddPatientWithoutTime/PatientWithoutTime.jsx
@@ -6,6 +6,12 @@ import {
   Input,
   Button,
   useDisclosure,
+  RadioGroup,
+  Stack,
+  Radio,
+  FormControl,
+  FormLabel,
+  Flex,
 } from "@chakra-ui/react";
 import { MdCancel } from "react-icons/md";
 import { firstGrid, firstBox, seccondBox } from "./style";
@@ -21,38 +27,51 @@ import {
 import { FaPlus } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { addCustomerWithOutTime } from "@/features/receptionDashboard/receptionDashboardSlice";
+
 const PatientWithoutTime = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-const [inputsData,setinputsData] = useState({
-  name: "",
-  last_name: "",
-  phone_number: "",
-  national_code: "",
-  address: "",
-  house_number: "",
-  drug_hist: true,
-  decease_hist: true,
-  doctor: "-",
-  offline_number:0
-})
+  const [inputsData, setinputsData] = useState({
+    name: "",
+    last_name: "",
+    phone_number: "",
+    national_code: "",
+    address: "",
+    house_number: "",
+    drug_hist: false,
+    decease_hist: false,
+    doctor: "-",
+    offline_number: 0,
+  });
 
-const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-const {token} = useSelector((store)=>store.adminDashboard)
+  const { token } = useSelector((store) => store.adminDashboard);
 
-const handleChange = (e)=>{
-const {name,value} = e.target;
+  // استیت برای مدیریت انتخاب Radio Button ها
+  const [drugHistory, setDrugHistory] = useState("fasle");
+  const [diseaseHistory, setDiseaseHistory] = useState("fasle");
 
-setinputsData((prev)=>({...prev,[name]:value}))
-}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setinputsData((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleRadioChange = (name, value) => {
+    setinputsData((prev) => ({ ...prev, [name]: value === "true" }));
+    if (name === "drug_hist") setDrugHistory(value);
+    if (name === "decease_hist") setDiseaseHistory(value);
+  };
+  console.log("inputsData:", inputsData);
 
-
-const handleSubmit= ()=>{
-dispatch(addCustomerWithOutTime({...inputsData,token}))
-onClose();
-}
-
+  const handleSubmit = () => {
+    dispatch(
+      addCustomerWithOutTime({
+        ...inputsData,
+        token,
+      })
+    );
+    onClose();
+  };
 
   return (
     <>
@@ -72,36 +91,92 @@ onClose();
         مراجع بین نوبت
       </Button>
 
-      <Modal  isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent p={6}>
-          
-            <Box sx={firstBox}>
-              
-                <Text fontWeight={"bold"}>مراجع جدید</Text>
-                <MdCancel cursor={'pointer'} onClick={onClose}/>
-              </Box>
+          <Box sx={firstBox}>
+            <Text fontWeight={"bold"}>مراجع جدید</Text>
+            <MdCancel cursor={"pointer"} onClick={onClose} />
+          </Box>
 
-              <Box
-              mb={4}
-                display={"flex"}
-                flexDirection={"column"}
-                gap={8}
-                width={"100%"}
-              >
-                <Input onChange={handleChange} name="name" placeholder="نام " size="md" />
-                <Input onChange={handleChange} name="last_name" placeholder=" نام خانوادگی" size="md" />
-                <Input onChange={handleChange} name="national_code" placeholder=" کد ملی" size="md" />
-                <Input onChange={handleChange} name="phone_number" placeholder=" شماره همراه" size="md" />
-                <Input onChange={handleChange} name="house_number" placeholder=" شماره ثابت" size="md" />
-                <Input onChange={handleChange} name="address" placeholder="  ادرس منزل" size="md" />
-              </Box>
+          <Box
+            mb={4}
+            display={"flex"}
+            flexDirection={"column"}
+            gap={8}
+            width={"100%"}
+          >
+            <Input
+              onChange={handleChange}
+              name="name"
+              placeholder="نام "
+              size="md"
+            />
+            <Input
+              onChange={handleChange}
+              name="last_name"
+              placeholder=" نام خانوادگی"
+              size="md"
+            />
+            <Input
+              onChange={handleChange}
+              name="national_code"
+              placeholder=" کد ملی"
+              size="md"
+            />
+            <Input
+              onChange={handleChange}
+              name="phone_number"
+              placeholder=" شماره همراه"
+              size="md"
+            />
 
-              <Box width={"100%"} mt={"4"}>
-                <Button onClick={handleSubmit} width={"100%"}>تایید اطلاعات و ادامه</Button>
-              </Box>
-      
-     
+            <Flex sx={{ flexWrap: "wrap" }}>
+              <FormControl as="fieldset" sx={{ flexBasis: 200, flexGrow: 1 }}>
+                <FormLabel as="legend">مصرف دارو:</FormLabel>
+                <RadioGroup
+                  onChange={(value) => handleRadioChange("drug_hist", value)}
+                  value={drugHistory}
+                >
+                  <Stack direction="row">
+                    <Radio value="true">دارد</Radio>
+                    <Radio value="false">ندارد</Radio>
+                  </Stack>
+                </RadioGroup>
+              </FormControl>
+
+              <FormControl as="fieldset" sx={{ flexBasis: 200, flexGrow: 1 }}>
+                <FormLabel as="legend">سابقه بیماری:</FormLabel>
+                <RadioGroup
+                  onChange={(value) => handleRadioChange("decease_hist", value)}
+                  value={diseaseHistory}
+                >
+                  <Stack direction="row">
+                    <Radio value="true">دارد</Radio>
+                    <Radio value="false">ندارد</Radio>
+                  </Stack>
+                </RadioGroup>
+              </FormControl>
+            </Flex>
+            <Input
+              onChange={handleChange}
+              name="house_number"
+              placeholder=" شماره ثابت"
+              size="md"
+            />
+            <Input
+              onChange={handleChange}
+              name="address"
+              placeholder="ادرس منزل"
+              size="md"
+            />
+          </Box>
+
+          <Box width={"100%"} mt={"4"}>
+            <Button onClick={handleSubmit} width={"100%"}>
+              تایید اطلاعات و ادامه
+            </Button>
+          </Box>
         </ModalContent>
       </Modal>
     </>
