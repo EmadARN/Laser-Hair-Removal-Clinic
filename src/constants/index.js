daf044aa1f457ab3a10c323f7b3482a5527fc497
RezaFarzipour <rezafafa78@gmail.com>
2024-10-30 12:50:18 +0330

    some data in constants changed

--- src/constants/index.js
+++ src/constants/index.js
@@ -417,17 +417,50 @@ export const UserData = [
   { id: 3, title: "مبلغ پرداخت شده", value: "50.000تومان " },
   { id: 3, title: "مبلغ قابل پرداخت", value: "200.000تومان " },
 ];
-export const dataForMap = [
-  { id: 1, title: "نام", value: "نام مرجع" },
-  { id: 2, title: "نام خانوادگی", value: "نام خانوادگی" },
-  { id: 3, title: "شماره موبایل", value: "09190978042" },
-  { id: 4, title: "تلفن ثابت", value: "024123455" },
-  { id: 5, title: "کدملی", value: "0312028156" },
-  { id: 6, title: "دارو مصرفی", value: "" },
-  { id: 7, title: "سابقه بیماری", value: "" },
-];
-export const TurnData = [
-  { id: 1, title: "روز و تاریخ", value: "سه شنبه ۲۶/۱۲/۱۴۰۲" },
-  { id: 2, title: "بازه زمانی", value: "9-11.30" },
-  { id: 3, title: "اپراتور", value: "نام اپراتور" },
-];
+export const dataForMap = (confrimInfoDetailCustomer, customerInformation) => {
+  return [
+    { id: 1, title: "نام", value: confrimInfoDetailCustomer.name || ""},
+    {
+      id: 2,
+      title: "نام خانوادگی",
+      value: confrimInfoDetailCustomer.last_name || ""
+    },
+    {
+      id: 3,
+      title: "شماره تلفن",
+      value: confrimInfoDetailCustomer.phone_number || ""
+    },
+    {
+      id: 4,
+      title: "کد ملی",
+      value: customerInformation.national_code || ""
+    },
+    {
+      id: 5,
+      title: "آدرس",
+      value: customerInformation.address || ""
+    },
+    {
+      id: 6,
+      title: " تلفن ثابت",
+      value: customerInformation.house_number|| ""
+    },
+    {
+      id: 7,
+      title: "سابقه بیماری",
+      value: customerInformation.drug_hist ? "بله" : "خیر",
+    },
+    {
+      id: 8,
+      title: "سابقه بیماری‌های جدی",
+      value: customerInformation.decease_hist ? "بله" : "خیر",
+    },
+  ];
+};
+export const TurnData = (date,slots,operatorName)=>{
+  return [
+    { id: 1, title: "روز و تاریخ", value: date || "" },
+    { id: 2, title: "بازه زمانی", value: slots || "" },
+    { id: 3, title: "اپراتور", value: operatorName || "" },
+  ];
+} 
