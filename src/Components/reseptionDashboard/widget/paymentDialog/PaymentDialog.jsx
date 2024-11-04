bc7c6ef27f7159e0373da700340f74ee867025f8
RezaFarzipour <rezafafa78@gmail.com>
2024-11-04 14:12:13 +0330

    lazer area modal added to paymen dialog modal

--- src/Components/reseptionDashboard/widget/paymentDialog/PaymentDialog.jsx
+++ src/Components/reseptionDashboard/widget/paymentDialog/PaymentDialog.jsx
@@ -13,9 +13,19 @@ import InformationBox from "./widget/InformationBox";
 import PaymentMethodSection from "./widget/PaymentMethodSection";
 import ConfitmTransaction from "./widget/ConfitmTransaction";
 import { extractTime } from "@/utils/extractTime";
+import { secBox } from "../LaserAreas/style";
+import LazerAreas from "../LaserAreas/LaserAreas";
+import { FaArrowRight } from "react-icons/fa";
 
 const PaymentDialog = ({ isOpen, onClose, reserve }) => {
   const [discountCode, setDiscountCode] = useState(""); // مدیریت حالت کد تخفیف
+  const [step, setStep] = useState(0);
+
+  const mdCancelHandler = () => {
+    setStep(0);
+
+    onClose();
+  };
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
@@ -35,53 +45,82 @@ const PaymentDialog = ({ isOpen, onClose, reserve }) => {
           width="100%"
         >
           <Text fontWeight="bold" fontSize={{ base: "lg", md: "xl" }}>
-            پرداخت
+            {step === 0 ? (
+              "پرداخت"
+            ) : (
+              <Box sx={secBox}>
+                <FaArrowRight
+                  onClick={() => setStep(step - 1)}
+                  cursor={"pointer"}
+                  fontSize={"17px"}
+                />
+                <Text mr={3} fontSize={"17px"} fontWeight={"bold"}>
+                  {" "}
+                  انتخاب نواحی
+                </Text>
+              </Box>
+            )}
           </Text>
           <MdCancel
             cursor="pointer"
-            onClick={onClose}
+            onClick={mdCancelHandler}
             fontSize={{ base: "20px", md: "24px" }}
           />
         </Flex>
 
         {/* Information Section */}
-        <Box width="100%" p={2} display="flex" flexDirection="column" gap={3}>
-          <InformationBox
-            title="نام و نام خانوادگی"
-            value={reserve.user || ""}
-          />
-          <InformationBox
-            title="زمان نوبت"
-            value={extractTime(reserve.reserve_time_str) || ""}
-          />
-          <InformationBox
-            title="ناحیه لیزر"
-            value={reserve.laser_area_name || ""}
-          />
-        </Box>
 
-        {/* Payment Method Section */}
-        <Box mt={3} width="100%" p={2}>
-          <PaymentMethodSection />
-        </Box>
+        {step === 0 ? (
+          <>
+            <Box
+              width="100%"
+              p={2}
+              display="flex"
+              flexDirection="column"
+              gap={3}
+            >
+              <InformationBox
+                title="نام و نام خانوادگی"
+                value={reserve.user || ""}
+              />
+              <InformationBox
+                title="زمان نوبت"
+                value={extractTime(reserve.reserve_time_str) || ""}
+              />
+              <InformationBox
+                setStep={setStep}
+                step={step}
+                title="ناحیه لیزر"
+                value={reserve.laser_area_name || ""}
+              />
+            </Box>
 
-        {/* Discount Code Section */}
-        <Box mt={4} width="100%" p={2}>
-          <Flex gap={2}>
-            <Input
-              placeholder="کد تخفیف "
-              value={discountCode}
-              onChange={(e) => setDiscountCode(e.target.value)}
-              size={{ base: "sm", md: "md" }}
-              flexBasis="50%"
-            />
-          </Flex>
-        </Box>
+            {/* Payment Method Section */}
+            <Box mt={3} width="100%" p={2}>
+              <PaymentMethodSection />
+            </Box>
 
-        {/* Confirm Transaction Section */}
-        <Box mt={4} width="100%">
-          <ConfitmTransaction reserve={reserve} />
-        </Box>
+            {/* Discount Code Section */}
+            <Box mt={4} width="100%" p={2}>
+              <Flex gap={2}>
+                <Input
+                  placeholder="کد تخفیف "
+                  value={discountCode}
+                  onChange={(e) => setDiscountCode(e.target.value)}
+                  size={{ base: "sm", md: "md" }}
+                  flexBasis="50%"
+                />
+              </Flex>
+            </Box>
+
+            {/* Confirm Transaction Section */}
+            <Box mt={4} width="100%">
+              <ConfitmTransaction reserve={reserve} />
+            </Box>
+          </>
+        ) : (
+          <LazerAreas onClose={onClose} setStep={setStep} />
+        )}
       </ModalContent>
     </Modal>
   );
